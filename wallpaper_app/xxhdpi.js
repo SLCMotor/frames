1f67d7d9e40a18813826e50a4a95ac41bbe9034a
Rob <slcbetatest@gmail.com>
2018-07-29 13:53:39 -0600

    New  images
    For dedicated wallpaper app

---
+++ wallpaper_app/xxhdpi.js
@@ -0,0 +1,219 @@
+{
+  "wallpapers": {
+    "category": [
+      {
+        "name": "Winter Mountains",
+        "wallpaper": [
+          {
+            "author": "SLCMotor",
+            "url": "http://www.slcmotor.com/userwalls/wallpaper/winter_01.jpg",
+            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/winter_01_thumb.jpg",
+            "name": "Big Cottonwood"
+          },
+          {
+            "author": "SLCMotor",
+            "url": "http://www.slcmotor.com/userwalls/wallpaper/winter_02.jpg",
+            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/winter_02_thumb.jpg",
+            "name": "Big Cottonwood"
+          },
+          {
+            "author": "SLCMotor",
+            "url": "http://www.slcmotor.com/userwalls/wallpaper/winter_03.jpg",
+            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/winter_03_thumb.jpg",
+            "name": "Big Cottonwood"
+          },
+          {
+            "author": "SLCMotor",
+            "url": "http://www.slcmotor.com/userwalls/wallpaper/winter_04.jpg",
+            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/winter_04_thumb.jpg",
+            "name": "Big Cottonwood"
+          },
+          {
+            "author": "SLCMotor",
+            "url": "http://www.slcmotor.com/userwalls/wallpaper/winter_05.jpg",
+            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/winter_05_thumb.jpg",
+            "name": "Big Cottonwood"
+          },
+          {
+            "author": "SLCMotor",
+            "url": "http://www.slcmotor.com/userwalls/wallpaper/winter_06.jpg",
+            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/winter_06_thumb.jpg",
+            "name": "Big Cottonwood"
+          },
+          {
+            "author": "SLCMotor",
+            "url": "http://www.slcmotor.com/userwalls/wallpaper/winter_07.jpg",
+            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/winter_07_thumb.jpg",
+            "name": "Big Cottonwood"
+          },
+          {
+            "author": "SLCMotor",
+            "url": "http://www.slcmotor.com/userwalls/wallpaper/winter_08.jpg",
+            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/winter_08_thumb.jpg",
+            "name": "Big Cottonwood"
+          },
+          {
+            "author": "SLCMotor",
+            "url": "http://www.slcmotor.com/userwalls/wallpaper/winter_09.jpg",
+            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/winter_09_thumb.jpg",
+            "name": "Big Cottonwood"
+          },
+          {
+            "author": "SLCMotor",
+            "url": "http://www.slcmotor.com/userwalls/wallpaper/winter_10.jpg",
+            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/winter_10_thumb.jpg",
+            "name": "Big Cottonwood"
+          },
+          {
+            "author": "SLCMotor",
+            "url": "http://www.slcmotor.com/userwalls/wallpaper/winter_11.jpg",
+            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/winter_11_thumb.jpg",
+            "name": "Big Cottonwood"
+          },
+          {
+            "author": "SLCMotor",
+            "url": "http://www.slcmotor.com/userwalls/wallpaper/winter_12.jpg",
+            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/winter_12_thumb.jpg",
+            "name": "Big Cottonwood"
+          },
+          {
+            "author": "SLCMotor",
+            "url": "http://www.slcmotor.com/userwalls/wallpaper/winter_13.jpg",
+            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/winter_13_thumb.jpg",
+            "name": "Big Cottonwood"
+          },
+          {
+            "author": "SLCMotor",
+            "url": "http://www.slcmotor.com/userwalls/wallpaper/winter_14.jpg",
+            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/winter_14_thumb.jpg",
+            "name": "Big Cottonwood"
+          },
+          {
+            "author": "SLCMotor",
+            "url": "http://www.slcmotor.com/userwalls/wallpaper/winter_15.jpg",
+            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/winter_15_thumb.jpg",
+            "name": "Big Cottonwood"
+          }         
+        ]
+      },           
+      {
+        "name": "Mountains",
+        "wallpaper": [
+          {
+            "author": "SLCMotor",
+            "url": "http://www.slcmotor.com/userwalls/wallpaper/summer_01.jpg",
+            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/summer_01_thumb.jpg",
+            "name": "Big Cottonwood"
+          },
+          {
+            "author": "SLCMotor",
+            "url": "http://www.slcmotor.com/userwalls/wallpaper/zions_01.jpg",
+            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/zions_01_thumb.jpg",
+            "name": "Zions"
+          },
+          {
+            "author": "SLCMotor",
+            "url": "http://www.slcmotor.com/userwalls/wallpaper/zions_02.jpg",
+            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/zions_02_thumb.jpg",
+            "name": "Zions"
+          },
+          {
+            "author": "SLCMotor",
+            "url": "http://www.slcmotor.com/userwalls/wallpaper/zions_03.jpg",
+            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/zions_03_thumb.jpg",
+            "name": "Zions"
+          },
+          {
+            "author": "SLCMotor",
+            "url": "http://www.slcmotor.com/userwalls/wallpaper/zions_04.jpg",
+            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/zions_04_thumb.jpg",
+            "name": "Zions"
+          },
+          {
+            "author": "SLCMotor",
+            "url": "http://www.slcmotor.com/userwalls/wallpaper/zions_05.jpg",
+            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/zions_05_thumb.jpg",
+            "name": "Zions"
+          },
+          {
+            "author": "SLCMotor",
+            "url": "http://www.slcmotor.com/userwalls/wallpaper/zions_06.jpg",
+            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/zions_06_thumb.jpg",
+            "name": "Zions"
+          },
+          {
+            "author": "SLCMotor",
+            "url": "http://www.slcmotor.com/userwalls/wallpaper/zions_07.jpg",
+            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/zions_07_thumb.jpg",
+            "name": "Zions"
+          },
+          {
+            "author": "SLCMotor",
+            "url": "http://www.slcmotor.com/userwalls/wallpaper/zions_08.jpg",
+            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/zions_08_thumb.jpg",
+            "name": "Zions"
+          },
+          {
+            "author": "SLCMotor",
+            "url": "http://www.slcmotor.com/userwalls/wallpaper/zions_09.jpg",
+            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/zions_09_thumb.jpg",
+            "name": "Zions"
+          },
+          {
+            "author": "SLCMotor",
+            "url": "http://www.slcmotor.com/userwalls/wallpaper/zions_10.jpg",
+            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/zions_10_thumb.jpg",
+            "name": "Zions"
+          },          
+          {
+            "author": "SLCMotor",
+            "url": "http://www.slcmotor.com/userwalls/wallpaper/zions_11.jpg",
+            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/zions_11_thumb.jpg",
+            "name": "Emerald Pools"
+          },
+          {
+            "author": "SLCMotor",
+            "url": "http://www.slcmotor.com/userwalls/wallpaper/disney_01.jpg",
+            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/disney_01_thumb.jpg",
+            "name": "Disney"
+          },
+          {
+            "author": "SLCMotor",
+            "url": "http://www.slcmotor.com/userwalls/wallpaper/utah_01.jpg",
+            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/utah_01_thumb.jpg",
+            "name": "Utah"
+          }         
+        ]
+      },
+      {
+        "name": "Grand Canyon",
+        "wallpaper": [
+          {
+            "author": "SLCMotor",
+            "url": "http://www.slcmotor.com/userwalls/wallpaper/grand_01.jpg",
+            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/grand_01_thumb.jpg",
+            "name": "Grand Canyon"
+          },
+          {
+            "author": "SLCMotor",
+            "url": "http://www.slcmotor.com/userwalls/wallpaper/grand_02.jpg",
+            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/grand_02_thumb.jpg",
+            "name": "Grand Canyon"
+          },
+          {
+            "author": "SLCMotor",
+            "url": "http://www.slcmotor.com/userwalls/wallpaper/grand_03.jpg",
+            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/grand_03_thumb.jpg",
+            "name": "Grand Canyon"
+          },
+          {
+            "author": "SLCMotor",
+            "url": "http://www.slcmotor.com/userwalls/wallpaper/grand_04.jpg",
+            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/grand_04_thumb.jpg",
+            "name": "Grand Canyon"
+          }
+        ]
+      }
+    ]
+  }
+}
