ca37837d865282d2210f17a77d76a328669f98a6
Rob <slcbetatest@gmail.com>
2018-07-29 13:56:55 -0600

    Updated path
    Updated path for json files

--- wallpaper_app/xxhdpi.js
+++ wallpaper_app/xxhdpi.js
@@ -6,92 +6,92 @@
         "wallpaper": [
           {
             "author": "SLCMotor",
-            "url": "http://www.slcmotor.com/userwalls/wallpaper/winter_01.jpg",
-            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/winter_01_thumb.jpg",
+            "url": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/winter_01.jpg",
+            "thumbUrl": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/winter_01_thumb.jpg",
             "name": "Big Cottonwood"
           },
           {
             "author": "SLCMotor",
-            "url": "http://www.slcmotor.com/userwalls/wallpaper/winter_02.jpg",
-            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/winter_02_thumb.jpg",
+            "url": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/winter_02.jpg",
+            "thumbUrl": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/winter_02_thumb.jpg",
             "name": "Big Cottonwood"
           },
           {
             "author": "SLCMotor",
-            "url": "http://www.slcmotor.com/userwalls/wallpaper/winter_03.jpg",
-            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/winter_03_thumb.jpg",
+            "url": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/winter_03.jpg",
+            "thumbUrl": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/winter_03_thumb.jpg",
             "name": "Big Cottonwood"
           },
           {
             "author": "SLCMotor",
-            "url": "http://www.slcmotor.com/userwalls/wallpaper/winter_04.jpg",
-            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/winter_04_thumb.jpg",
+            "url": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/winter_04.jpg",
+            "thumbUrl": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/winter_04_thumb.jpg",
             "name": "Big Cottonwood"
           },
           {
             "author": "SLCMotor",
-            "url": "http://www.slcmotor.com/userwalls/wallpaper/winter_05.jpg",
-            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/winter_05_thumb.jpg",
+            "url": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/winter_05.jpg",
+            "thumbUrl": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/winter_05_thumb.jpg",
             "name": "Big Cottonwood"
           },
           {
             "author": "SLCMotor",
-            "url": "http://www.slcmotor.com/userwalls/wallpaper/winter_06.jpg",
-            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/winter_06_thumb.jpg",
+            "url": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/winter_06.jpg",
+            "thumbUrl": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/winter_06_thumb.jpg",
             "name": "Big Cottonwood"
           },
           {
             "author": "SLCMotor",
-            "url": "http://www.slcmotor.com/userwalls/wallpaper/winter_07.jpg",
-            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/winter_07_thumb.jpg",
+            "url": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/winter_07.jpg",
+            "thumbUrl": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/winter_07_thumb.jpg",
             "name": "Big Cottonwood"
           },
           {
             "author": "SLCMotor",
-            "url": "http://www.slcmotor.com/userwalls/wallpaper/winter_08.jpg",
-            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/winter_08_thumb.jpg",
+            "url": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/winter_08.jpg",
+            "thumbUrl": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/winter_08_thumb.jpg",
             "name": "Big Cottonwood"
           },
           {
             "author": "SLCMotor",
-            "url": "http://www.slcmotor.com/userwalls/wallpaper/winter_09.jpg",
-            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/winter_09_thumb.jpg",
+            "url": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/winter_09.jpg",
+            "thumbUrl": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/winter_09_thumb.jpg",
             "name": "Big Cottonwood"
           },
           {
             "author": "SLCMotor",
-            "url": "http://www.slcmotor.com/userwalls/wallpaper/winter_10.jpg",
-            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/winter_10_thumb.jpg",
+            "url": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/winter_10.jpg",
+            "thumbUrl": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/winter_10_thumb.jpg",
             "name": "Big Cottonwood"
           },
           {
             "author": "SLCMotor",
-            "url": "http://www.slcmotor.com/userwalls/wallpaper/winter_11.jpg",
-            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/winter_11_thumb.jpg",
+            "url": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/winter_11.jpg",
+            "thumbUrl": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/winter_11_thumb.jpg",
             "name": "Big Cottonwood"
           },
           {
             "author": "SLCMotor",
-            "url": "http://www.slcmotor.com/userwalls/wallpaper/winter_12.jpg",
-            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/winter_12_thumb.jpg",
+            "url": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/winter_12.jpg",
+            "thumbUrl": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/winter_12_thumb.jpg",
             "name": "Big Cottonwood"
           },
           {
             "author": "SLCMotor",
-            "url": "http://www.slcmotor.com/userwalls/wallpaper/winter_13.jpg",
-            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/winter_13_thumb.jpg",
+            "url": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/winter_13.jpg",
+            "thumbUrl": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/winter_13_thumb.jpg",
             "name": "Big Cottonwood"
           },
           {
             "author": "SLCMotor",
-            "url": "http://www.slcmotor.com/userwalls/wallpaper/winter_14.jpg",
-            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/winter_14_thumb.jpg",
+            "url": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/winter_14.jpg",
+            "thumbUrl": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/winter_14_thumb.jpg",
             "name": "Big Cottonwood"
           },
           {
             "author": "SLCMotor",
-            "url": "http://www.slcmotor.com/userwalls/wallpaper/winter_15.jpg",
-            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/winter_15_thumb.jpg",
+            "url": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/winter_15.jpg",
+            "thumbUrl": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/winter_15_thumb.jpg",
             "name": "Big Cottonwood"
           }         
         ]
@@ -101,86 +101,86 @@
         "wallpaper": [
           {
             "author": "SLCMotor",
-            "url": "http://www.slcmotor.com/userwalls/wallpaper/summer_01.jpg",
-            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/summer_01_thumb.jpg",
+            "url": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/summer_01.jpg",
+            "thumbUrl": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/summer_01_thumb.jpg",
             "name": "Big Cottonwood"
           },
           {
             "author": "SLCMotor",
-            "url": "http://www.slcmotor.com/userwalls/wallpaper/zions_01.jpg",
-            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/zions_01_thumb.jpg",
+            "url": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/zions_01.jpg",
+            "thumbUrl": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/zions_01_thumb.jpg",
             "name": "Zions"
           },
           {
             "author": "SLCMotor",
-            "url": "http://www.slcmotor.com/userwalls/wallpaper/zions_02.jpg",
-            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/zions_02_thumb.jpg",
+            "url": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/zions_02.jpg",
+            "thumbUrl": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/zions_02_thumb.jpg",
             "name": "Zions"
           },
           {
             "author": "SLCMotor",
-            "url": "http://www.slcmotor.com/userwalls/wallpaper/zions_03.jpg",
-            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/zions_03_thumb.jpg",
+            "url": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/zions_03.jpg",
+            "thumbUrl": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/zions_03_thumb.jpg",
             "name": "Zions"
           },
           {
             "author": "SLCMotor",
-            "url": "http://www.slcmotor.com/userwalls/wallpaper/zions_04.jpg",
-            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/zions_04_thumb.jpg",
+            "url": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/zions_04.jpg",
+            "thumbUrl": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/zions_04_thumb.jpg",
             "name": "Zions"
           },
           {
             "author": "SLCMotor",
-            "url": "http://www.slcmotor.com/userwalls/wallpaper/zions_05.jpg",
-            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/zions_05_thumb.jpg",
+            "url": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/zions_05.jpg",
+            "thumbUrl": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/zions_05_thumb.jpg",
             "name": "Zions"
           },
           {
             "author": "SLCMotor",
-            "url": "http://www.slcmotor.com/userwalls/wallpaper/zions_06.jpg",
-            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/zions_06_thumb.jpg",
+            "url": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/zions_06.jpg",
+            "thumbUrl": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/zions_06_thumb.jpg",
             "name": "Zions"
           },
           {
             "author": "SLCMotor",
-            "url": "http://www.slcmotor.com/userwalls/wallpaper/zions_07.jpg",
-            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/zions_07_thumb.jpg",
+            "url": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/zions_07.jpg",
+            "thumbUrl": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/zions_07_thumb.jpg",
             "name": "Zions"
           },
           {
             "author": "SLCMotor",
-            "url": "http://www.slcmotor.com/userwalls/wallpaper/zions_08.jpg",
-            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/zions_08_thumb.jpg",
+            "url": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/zions_08.jpg",
+            "thumbUrl": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/zions_08_thumb.jpg",
             "name": "Zions"
           },
           {
             "author": "SLCMotor",
-            "url": "http://www.slcmotor.com/userwalls/wallpaper/zions_09.jpg",
-            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/zions_09_thumb.jpg",
+            "url": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/zions_09.jpg",
+            "thumbUrl": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/zions_09_thumb.jpg",
             "name": "Zions"
           },
           {
             "author": "SLCMotor",
-            "url": "http://www.slcmotor.com/userwalls/wallpaper/zions_10.jpg",
-            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/zions_10_thumb.jpg",
+            "url": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/zions_10.jpg",
+            "thumbUrl": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/zions_10_thumb.jpg",
             "name": "Zions"
           },          
           {
             "author": "SLCMotor",
-            "url": "http://www.slcmotor.com/userwalls/wallpaper/zions_11.jpg",
-            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/zions_11_thumb.jpg",
+            "url": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/zions_11.jpg",
+            "thumbUrl": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/zions_11_thumb.jpg",
             "name": "Emerald Pools"
           },
           {
             "author": "SLCMotor",
-            "url": "http://www.slcmotor.com/userwalls/wallpaper/disney_01.jpg",
-            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/disney_01_thumb.jpg",
+            "url": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/disney_01.jpg",
+            "thumbUrl": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/disney_01_thumb.jpg",
             "name": "Disney"
           },
           {
             "author": "SLCMotor",
-            "url": "http://www.slcmotor.com/userwalls/wallpaper/utah_01.jpg",
-            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/utah_01_thumb.jpg",
+            "url": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/utah_01.jpg",
+            "thumbUrl": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/utah_01_thumb.jpg",
             "name": "Utah"
           }         
         ]
@@ -190,26 +190,26 @@
         "wallpaper": [
           {
             "author": "SLCMotor",
-            "url": "http://www.slcmotor.com/userwalls/wallpaper/grand_01.jpg",
-            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/grand_01_thumb.jpg",
+            "url": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/grand_01.jpg",
+            "thumbUrl": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/grand_01_thumb.jpg",
             "name": "Grand Canyon"
           },
           {
             "author": "SLCMotor",
-            "url": "http://www.slcmotor.com/userwalls/wallpaper/grand_02.jpg",
-            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/grand_02_thumb.jpg",
+            "url": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/grand_02.jpg",
+            "thumbUrl": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/grand_02_thumb.jpg",
             "name": "Grand Canyon"
           },
           {
             "author": "SLCMotor",
-            "url": "http://www.slcmotor.com/userwalls/wallpaper/grand_03.jpg",
-            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/grand_03_thumb.jpg",
+            "url": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/grand_03.jpg",
+            "thumbUrl": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/grand_03_thumb.jpg",
             "name": "Grand Canyon"
           },
           {
             "author": "SLCMotor",
-            "url": "http://www.slcmotor.com/userwalls/wallpaper/grand_04.jpg",
-            "thumbUrl": "http://www.slcmotor.com/userwalls/wallpaper/grand_04_thumb.jpg",
+            "url": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/grand_04.jpg",
+            "thumbUrl": "https://raw.githubusercontent.com/SLCMotor/frames/master/wallpaper_app/grand_04_thumb.jpg",
             "name": "Grand Canyon"
           }
         ]
